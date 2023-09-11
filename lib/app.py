1463937d3309c8841165681c1aec12b6e569c9aa
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-09-11 18:01:20 +0300

    starts assignment

---
+++ lib/app.py
@@ -0,0 +1,6 @@
+# lib/app.py
+print("Hello world!", end=" ")
+print("Hello Earth!", end="!")
+print("Hello Universe!", end="!!")
+print("Hello Dunia!", end="!!!\n")
+# print("Hello Ulimwengu!", end=" ")
